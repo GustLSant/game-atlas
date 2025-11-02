dec9cc01d500f50c0d56a6a8052b44c67e2e15ed
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-02 00:10:29 -0300

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <p class="text-red-500">Texto vermelho para testar o tailwind</p>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
